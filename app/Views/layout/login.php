1c4761840e99c26a1f2348556134c759e58ed255
royyanfathullah <156770481+royyanfathullah@users.noreply.github.com>
2024-04-30 13:32:49 +0700

    initial commit

---
+++ app/Views/layout/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Slide Navbar</title>
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/login.css"/>
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?= $this->renderSection('login'); ?>
+</body>
+</html>
---
+++ app/Views/layout/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/navbar.css">
+  <title>
+    123
+  </title>
+</head>
+<body>
+  <?= $this->include('layout/navbar'); ?>
+    <?= $this->renderSection('content'); ?>
+</body>
+</html>
