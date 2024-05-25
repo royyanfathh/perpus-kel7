eff917ed22550b0b79b816a5c571dbeb45442d5a
royyanfathullah <156770481+royyanfathullah@users.noreply.github.com>
2024-05-25 23:28:06 +0700

    merapihkan navbar

--- app/Views/pages/daftarbuku.php
+++ app/Views/pages/daftarbuku.php
@@ -1,50 +1,55 @@
 <?= $this->extend('layout/daftarbuku'); ?>
 <?= $this->section('daftarbuku'); ?>
 <div class="container">
-				<div class="row">
-					<div class="col-12">
-						<div class="section-title">
-							<br>
-							<h2>Daftar Buku</h2>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-12">
-						<div class="product-info">
-							<div class="tab-content" id="myTabContent">
-								<!-- Start Single Tab -->
-								<div class="tab-pane fade show active" id="man" role="tabpanel">
-									<div class="tab-single">
-										<div class="row">
-											<div class="col-xl-3 col-lg-4 col-md-4 col-12">
-												<div class="single-product">
-													<div class="product-img">
-														<a href="product-details.html">
-															<img class="default-img" src="https://via.placeholder.com/550x750" alt="#">
-															<img class="hover-img" src="https://via.placeholder.com/550x750" alt="#">
-														</a>
-														<div class="button-head">
-															<div class="product-action">
-																<a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i class=" ti-eye"></i><span>Quick Shop</span></a>
-																<a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
-																<a title="Compare" href="#"><i class="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
-															</div>
-															<div class="product-action-2">
-																<a title="Add to cart" href="#">Add to cart</a>
-															</div>
-														</div>
-													</div>
-													<div class="product-content">
-														<h3><a href="product-details.html">Women Hot Collection</a></h3>
-														<div class="product-price">
-															<span>$29.00</span>
-														</div>
-													</div>
+	<a href="<?= base_url(); ?>" class="btn btn-outline-primary m-3 mt-4 position-absolute">
+		<i class="ti ti-home"></i>
+		Home
+	</a>
+	<div class="row">
+		<div class="col-12">
+			<div class="section-title">
+				<br>
+				<h2>Daftar Buku</h2>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<div class="product-info">
+				<div class="tab-content" id="myTabContent">
+					<!-- Start Single Tab -->
+					<div class="tab-pane fade show active" id="man" role="tabpanel">
+						<div class="tab-single">
+							<div class="row">
+								<div class="col-xl-3 col-lg-4 col-md-4 col-12">
+									<div class="single-product">
+										<div class="product-img">
+											<a href="product-details.html">
+												<img class="default-img" src="https://via.placeholder.com/550x750" alt="#">
+												<img class="hover-img" src="https://via.placeholder.com/550x750" alt="#">
+											</a>
+											<div class="button-head">
+												<div class="product-action">
+													<a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i
+															class=" ti-eye"></i><span>Quick Shop</span></a>
+													<a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
+													<a title="Compare" href="#"><i class="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
 												</div>
+												<div class="product-action-2">
+													<a title="Add to cart" href="#">Add to cart</a>
+												</div>
+											</div>
+										</div>
+										<div class="product-content">
+											<h3><a href="product-details.html">Women Hot Collection</a></h3>
+											<div class="product-price">
+												<span>$29.00</span>
 											</div>
 										</div>
 									</div>
 								</div>
-								<!--/ End Single Tab -->
-                                <?= $this->endSection();
+							</div>
+						</div>
+					</div>
+					<!--/ End Single Tab -->
+					<?= $this->endSection();
