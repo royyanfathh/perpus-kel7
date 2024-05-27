f10d4cda664c51d4f273a7eee66f9f640638d2af
royyanfathullah <156770481+royyanfathullah@users.noreply.github.com>
2024-05-27 09:37:56 +0700

    menambahkan css

--- app/Views/pages/daftarbuku.php
+++ app/Views/pages/daftarbuku.php
@@ -29,11 +29,7 @@
 											</a>
 											<div class="button-head">
 												<div class="product-action">
-													<a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i
-															class=" ti-eye"></i><span>Quick Shop</span></a>
-													<a title="Wishlist" href="#"><i class="ti-heart"></i><span>Add to Wishlist</span></a>
-													<a title="Compare" href="#"><i class="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
-															class=" ti-eye"></i><span>Ajukan Pinjaman</span></a>
+													<a	class=" ti-eye"></i><span>Ajukan Pinjaman</span></a>
 												</div>
 												<div class="product-action-2">
 													<a title="Add to cart" href="/dashboard">Pinjam Sekarang!</a>
